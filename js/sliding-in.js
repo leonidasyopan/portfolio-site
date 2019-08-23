dfe8c28381f5833517d6f65e9ea369368e40c62b
Leonidas Yopan <leonidasyopan@gmail.com>
2019-08-23 17:26:26 -0300

    Deleted the Slide In JS application

--- js/sliding-in.js
+++ js/sliding-in.js
@@ -1,30 +1,37 @@
-const profileHeadings = document.querySelector(#profile-headings);
-const profilePhoto = document.querySelector(#profile-picture);
-const profileDescription = document.querySelector(#professional-description);
+const profileHeadings = document.querySelector('#profile-headings');
+const profilePhoto = document.querySelector('#profile-picture');
+const profileDescription = document.querySelector('#professional-description');
 
-function debounce(func, wait = 20, immediate = true) {
+function debounce(func, wait, immediate) {
     var timeout;
-    return function() {
+
+    return function executedFunction() {
         var context = this;
         var args = arguments;
+            
         var later = function() {
-            timeout = null;
-            if(!immediate) func.apply(context, args);
+        timeout = null;
+        if (!immediate) func.apply(context, args);
         };
+
         var callNow = immediate && !timeout;
+        
         clearTimeout(timeout);
+
         timeout = setTimeout(later, wait);
-        if(callNow) func.apply(context, args);
+        
+        if (callNow) func.apply(context, args);
     };
 };
 
+
 function checkSlide(item) {
     // half way through the item
-    const slideInAt = (window.scrollY + window.innerHeight) / item.height / 2;
+    const slideInAt = (window.scrollY + window.innerHeight) - item.height / 2;
     // bottom of the item
     const itemBottom = item.offsetTop + item.height;
     // item is hal shown
-    const = isHalfShown = slideInAt > item.offsetTop;
+    const isHalfShown = slideInAt > item.offsetTop;
     // item is not scrolled past
     const isNotScrolledPast = window.scrollY < itemBottom;
 
@@ -35,10 +42,6 @@ function checkSlide(item) {
     }
 }
 
-window.addEventListener('scroll', debounce(checkSlide));
-
-checkSlide(profileHeadings);
-checkSlide(profilePhoto);
-checkSlide(profileDescription);
-
-
+window.addEventListener('scroll', debounce(checkSlide(profileHeadings)));
+window.addEventListener('scroll', debounce(checkSlide(profilePhoto)));
+window.addEventListener('scroll', debounce(checkSlide(profileDescription)));
